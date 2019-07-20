704f5fe9328d4c59bca982bd043ec9be45fedab6
Oliver Cripps <ollycripps@gmail.com>
2019-07-20 15:37:21 +0100

    Create Users

---
+++ lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'user.dart';
+import 'post.dart';
+import 'comment.dart';
+
+
+
+User follower1 = new User('thierry_henry', AssetImage('lib/assets/thierr_henry.jpg'), [], [], [], [], true);
+User follower2 = new User('em_rata', AssetImage('lib/assets/em_rata.jpg'), [], [], [], [], true);
+User follower3 = new User('anthony_joshua', AssetImage('lib/assets/anthony_joshua.jpg'), [], [], [], [], false);
